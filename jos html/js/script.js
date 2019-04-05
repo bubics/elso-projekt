076750cd47431f03a5c294a3b337257fac18c9e3
bubics <49301657+bubics@users.noreply.github.com>
2019-04-05 08:44:42 +0200

    Add files via upload

---
+++ jos html/js/script.js
@@ -0,0 +1,10 @@
+﻿	var nevek = ["V-bucks", "Barna", "Laura" , "Életkedv","A lolosok buzik"];
+
+	var lany;
+
+	function joslas() {
+		var i;
+		i = Math.floor(Math.random()*5);
+		lany = nevek[i];
+		document.getElementById("eredmeny").innerHTML = lany;
+	}
---
+++ jos html/js/script2.js
@@ -0,0 +1,10 @@
+﻿var nevek = ["Harmónia", "Megvilágosodás", "Pozitivitás" , "Tudod hova kell menned vasárnap","Almáspite"];
+
+	var lany;
+
+	function joslas() {
+		var i;
+		i = Math.floor(Math.random()*5);
+		lany = nevek[i];
+		document.getElementById("eredmeny").innerHTML = lany;
+	}
